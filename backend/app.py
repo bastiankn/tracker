b812c1ac2ddcd81159bd6f63c802f3f850a242fd
bastiankn <115536160+bastiankn@users.noreply.github.com>
2024-02-18 19:39:18 +0100

    fixed creating an instance

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,7 @@ def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+    db.init_app(app)
     return app
 
 db = SQLAlchemy()
--- backend/init_db.py
+++ backend/init_db.py
@@ -1,5 +1,7 @@
 from app import db, create_app
 
 app = create_app()
-db.drop_all()
-db.create_all()
+
+with app.app_context():
+    db.drop_all()   
+    db.create_all() 
